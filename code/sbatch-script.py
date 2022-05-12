5af0ade9d3651d866e05b649527044facb468e5f
Bhargav Acharya <bhargav.acharya@uni-bielefeld.de>
2022-05-12 11:59:05 +0200

    Update to run multistep on gpu

--- code/sbatch-script.py
+++ code/sbatch-script.py
@@ -15,20 +15,19 @@ for i in range(nums_jobs):
 	print(f"submiting job {i}")
 	if i == 0:
 		instruction = ["sbatch",f"--output={dataset}-{i}-multistep.log",
-					   f"--gpus={gpus}",f"--expname={exp_name}-{i}",
-					   f"--data={data_path}",
-					   f"--save_dir={save_chekpoints}",
-					   f"--temporal={temp}",
-					   f"--initial=0",
+					   f"--gpus={gpus}",f"--export=expname={exp_name}-{i}",
+					   f"data={data_path}",
+					   f"save_dir={save_chekpoints}",
+					   f"temporal={temp}",
+					   f"initial=0",
 					   f"train-multistep.sh"]
 	else:
 		instruction = ["sbatch",f"--output={dataset}-{i}-multistep.log",
-					   f"--gpus={gpus}",f"--expname={exp_name}-{i}",
-					   f"--data={data_path}",
-					   f"--save_dir={save_chekpoints}",
-					   f"--temporal={temp}",
-					   f"--initial=0",
-					   f"--dependency=afterok:{batch_id}",
+					   f"--gpus={gpus}",f"--export=expname={exp_name}-{i}",
+					   f"data={data_path}",
+					   f"save_dir={save_chekpoints}",
+					   f"temporal={temp}",
+					   f"initial=0",
 					   f"train-multistep.sh"]
 	output = subprocess.run(instruction,capture_output=True)
 	print(output)
